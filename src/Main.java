81c44ebe69ef38524887b2249ad6037b7d8c602b
shaft163rus <shaft163rus@gmail.com>
2023-06-11 11:30:48 +0400

    first_try

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import exceptions.WrongLoginException;
+import exceptions.WrongPasswordException;
+
 public class Main {
+    private static final String ALLOWED_CHARS = "^[a-zA-Z0-9_]{1,20}$";
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try{
+            checkPassword("admin", "qwerty", "qwerty");
+        } catch (WrongLoginException | WrongPasswordException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void checkPassword(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
+
+        if (!login.matches(ALLOWED_CHARS)) {
+            throw new WrongLoginException("Login is incorrect");
+        }
+
+        if (!password.matches(ALLOWED_CHARS)) {
+            throw new WrongPasswordException("Password is incorrect");
+        }
+
+        if (!confirmPassword.equals(password)) {
+            throw new WrongPasswordException("Passwords not matched");
+        }
+
     }
 }
